c0e58e2788a13e681825b6f5fece4300bc0c549a
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-03 19:21:32 -0400

    refractoring cors

--- server/index.js
+++ server/index.js
@@ -9,19 +9,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //CORS Middleware
-app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
-});
+app.use(require('./middleware/cors'))
 
 // Routes
 const generalRoutes = require("./routes/generalRoutes");
 const loginRoutes = require("./routes/loginRoutes")
 app.use(generalRoutes);
 app.use(loginRoutes);
+
 // Listen
 app.listen(PORT, () => {
     console.log("Server is running in port", PORT);
---
+++ server/middleware/cors.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+module.exports = function(req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+}
