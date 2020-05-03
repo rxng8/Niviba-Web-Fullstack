95636fdbdaf0bb5838fe45dd690645d34256b344
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-03 15:52:53 -0400

    config for database setup

---
+++ server/db/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    user: 'DESKTOP-36V82EH\\ALEXNGUYEN',
+    password: '',
+    server: 'localhost', 
+    database: 'Test',
+    port: 1433,
+    options: {
+        encrypt: false
+    }
+}
