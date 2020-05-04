2a555e05f12fe208d0f471ec84839c8e6cd5a1b8
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-04 02:24:02 -0400

    add mongo db config and setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const bodyParser = require('body-parser');
+const InitiateMongoServer = require("./db/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,9 +19,11 @@ app.use(require('./middleware/cors'))
 const generalRoutes = require("./routes/generalRoutes");
 const loginRoutes = require("./routes/loginRoutes");
 const registerRoutes = require("./routes/loginRoutes");
+const userMongo = require('./routes/user(mongo)');
 app.use(generalRoutes);
 app.use(loginRoutes);
 app.use(registerRoutes);
+app.use(userMongo);
 
 // Listen
 app.listen(PORT, () => {
