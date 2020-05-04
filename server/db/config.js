ae1242c1d33206f739e8c4cb23dac1a383db1b8a
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-04 02:24:39 -0400

    refractoring to both sql and mongodb config

--- server/db/config.js
+++ server/db/config.js
@@ -1,4 +1,4 @@
-module.exports = {
+exports.sql = {
     user: 'DESKTOP-36V82EH\\ALEXNGUYEN',
     password: '',
     server: 'localhost', 
@@ -7,4 +7,9 @@ module.exports = {
     options: {
         encrypt: false
     }
+}
+ 
+exports.mongo = {
+    "connectionString": "mongodb://localhost/Test",
+    "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
 }
