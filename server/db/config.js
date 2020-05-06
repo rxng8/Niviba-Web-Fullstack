c6d535118624c5e4bb002ac56c79cdec4588c07e
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-06 12:20:18 -0400

    clean sql

--- server/db/config.js
+++ server/db/config.js
@@ -11,5 +11,6 @@ exports.sql = {
  
 exports.mongo = {
     "connectionString": "mongodb://mongo:27017/Test",
+    // "connectionString": "mongodb://localhost:27017/Test",
     "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
 }
