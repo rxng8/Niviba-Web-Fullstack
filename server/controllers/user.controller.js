cd4b990e8c627d133fbe5bdaac1da959f5116711
Alex Nguyen <alexvn.edu@gmail.com>
2020-05-13 20:52:20 -0400

    controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,159 @@
+const UserModel = require('../models/user.model.js');
+const { check, validationResult} = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+// Create and Save a new Note
+exports.create = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+    try {
+        let user = await UserModel.findOne({
+            email
+        });
+        if (user) {
+            return res.status(400).json({
+                msg: "User Already Exists"
+            });
+        }
+
+        user = new UserModel({
+            username,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            "randomString", {
+                expiresIn: 10000
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.status(200).json({
+                    token
+                });
+            }
+        );
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Error in Saving");
+    }
+};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    UserModel.find()
+    .then(users => {
+        res.send(users);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving users."
+        });
+    });
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+    UserModel.findById(req.params.userId)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });            
+        }
+        res.send(user);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving user with id " + req.params.userId
+        });
+    });
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "User content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+    UserModel.findByIdAndUpdate(req.params.userId, {
+        username:username,
+        email:email,
+        password:password
+    }, {new: true})
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating user with id " + req.params.userId
+        });
+    });
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+    UserModel.findByIdAndRemove(req.params.userId)
+    .then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });
+        }
+        res.send({message: "Note deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "User not found with id " + req.params.userId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete note with id " + req.params.userId
+        });
+    });
+};
