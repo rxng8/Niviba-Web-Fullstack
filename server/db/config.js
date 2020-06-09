8410c6079220ee24964cc05c515c4c9a8667cb4b
Alex Nguyen <alexvn.edu@gmail.com>
2020-06-09 00:35:53 -0400

    refractor file

--- server/db/config.js
+++ server/db/config.js
@@ -7,3 +7,8 @@ exports.mongo_with_docker = {
     "connectionString": "mongodb://mongo:27017/Test",
     "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
 }
+
+exports.mongo_production = {
+    "connectionString": "mongodb+srv://rxng:dung1234@cluster0-fufvv.mongodb.net/Niviba?retryWrites=true&w=majority",
+    "secret": "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
+}
