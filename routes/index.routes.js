2875821d7f2554d59856f5cf001546def187b1c3
Alex Nguyen <alexvn.edu@gmail.com>
2020-06-18 23:44:53 -0400

    change index page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -16,7 +16,7 @@ app.get("/",(req, res, next) => {
     // executeQuery(res, config, qurey);
 
     console.log("Get here at generateRoutes/homepage after execute query method!");
-    res.send("<h1>Helloodfvdfbooo</h1>");
+    res.send("<h1>Hello, This is the index page! Please navigate to <a href='https://github.com/rxng8/Niviba-Web-Fullstack'>This Link</a> to see the documentation for testing!</h1>");
     // next();
 });
 
