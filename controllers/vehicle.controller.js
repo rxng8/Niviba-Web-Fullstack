d8c6415b8d2e7ee8483ee90d0fecd1e22a29c6e8
Alex Nguyen <alexvn.edu@gmail.com>
2020-06-24 01:10:33 -0400

    fix bug #56

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -1,5 +1,32 @@
 const VehicleModel = require('../models/vehicle.model.js');
 const {check, validationResult} = require("express-validator/check");
+const DepotModel = require('../models/depot.model.js');
+
+const updateToDepots = function (depots) {
+    for (depotObject in depots) {
+        try {
+            let depot = DepotModel.find({id: depotObject.id});
+            if (!depot) {
+                return res.status(404).send({
+                    message: "Depot not found with id " + depotObject.id
+                }); 
+            }
+            const {
+                id,
+                name,
+                capacity,
+                address,
+                coordinates,
+                time_window,
+                vehicle
+            } = req.body
+        } catch(err) {
+            console.log(err.message);
+            res.status(500).send("Error in finding depot to insert vehicle!");
+            continue;
+        }
+    }
+}
 
 exports.create = async (req, res) => {
     const {
@@ -89,42 +116,42 @@ exports.update = (req, res) => {
         vehicle_capacity: vehicle_capacity,
         depot: depot
     }, {new: true})
-    .then(depot => {
-        if(!depot) {
+    .then(vehicle => {
+        if(!vehicle) {
             return res.status(404).send({
-                message: "Depot not found with id " + req.params.vehicleId
+                message: "Vehicle not found with id " + req.params.vehicleId
             });
         }
-        res.send(note);
+        res.send(vehicle);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Depot not found with id " + req.params.vehicleId
+                message: "Vehicle not found with id " + req.params.vehicleId
             });                
         }
         return res.status(500).send({
-            message: "Error updating user with id " + req.params.vehicleId
+            message: "Error updating vehicle with id " + req.params.vehicleId
         });
     });
 };
 
 exports.delete = (req, res) => {
-    DepotModel.findByIdAndRemove(req.params.vehicleId)
-    .then(depot => {
-        if(!depot) {
+    VehicleModel.findByIdAndRemove(req.params.vehicleId)
+    .then(vehicle => {
+        if(!vehicle) {
             return res.status(404).send({
-                message: "depot not found with id " + req.params.vehicleId
+                message: "Vehicle not found with id " + req.params.vehicleId
             });
         }
-        res.send({message: "depot deleted successfully!"});
+        res.send({message: "Vehicle deleted successfully!"});
     }).catch(err => {
         if(err.kind === 'ObjectId' || err.name === 'NotFound') {
             return res.status(404).send({
-                message: "depot not found with id " + req.params.vehicleId
+                message: "Vehicle not found with id " + req.params.vehicleId
             });
         }
         return res.status(500).send({
-            message: "Could not delete depot with id " + req.params.vehicleId
+            message: "Could not delete vehicle with id " + req.params.vehicleId
         });
     });
 };
