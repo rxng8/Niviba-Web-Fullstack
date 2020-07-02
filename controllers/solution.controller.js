4766a9bd174b1b04e55beb67f3e76eec4d9085ef
Alex Nguyen <alexvn.edu@gmail.com>
2020-07-02 13:42:29 -0400

    optimizer api

--- controllers/solution.controller.js
+++ controllers/solution.controller.js
@@ -1,17 +1,38 @@
 const axios = require('axios');
 require("express-validator/check");
+const request = require('request');
 
 // Solution controller
 
 exports.solve = async (req, res) => {
-    let url = "#";
-    await axios.get(url)
-        .then((res) => {
-            console.log("Made a request to" + url);
+    let url = "http://localhost:5555/routing";
+    // let url = "http://7b857d957582.ngrok.io/routing"
+    let request_data_JSON = req.body;
+    let request_data = JSON.stringify(request_data_JSON);
+    // console.log(request_data);
+    // res.send(request_data)
+    const options = {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        data: request_data,
+        url: url,
+    };
+    
+    let solution_object;
+
+    await axios(options)
+        .then((solution) => {
+            console.log("Making a request to " + url);
+            solution_object = solution;
         })
         .catch((err) => {
             console.log(err);
         });
 
-    res.send("<h2>Hello</h2>");
+    if (solution_object) {
+        console.log(solution_object.data)
+        res.send(solution_object.data);
+    } else {
+        res.send("<h2>Failed to make request to the AI server</h2>");
+    }
 }
